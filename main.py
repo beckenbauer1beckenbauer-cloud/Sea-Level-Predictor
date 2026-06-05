3687646fbcf35510a43dc0145e48fb75029f3a9e
codingpro3 <mafhahouamjmamaham@gmail.com>
2026-06-05 14:48:34 +0100

    Add script to generate plot and run unit tests
    This script generates a plot and runs unit tests for the sea level predictor.

---
+++ main.py
@@ -0,0 +1,11 @@
+import sea_level_predictor
+import unittest
+
+# Test your function by generating the plot
+print("Generating plot...")
+sea_level_predictor.draw_plot()
+print("Plot successfully saved as 'sea_level_plot.png'.\n")
+
+# Run unit tests automatically
+print("Running unit tests...")
+unittest.main()
